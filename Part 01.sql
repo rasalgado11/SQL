7ddab4dcc86528c5008d53e60be25347bf90dedf
Raissa Salgado <107374219+rasalgado11@users.noreply.github.com>
2024-02-18 00:44:15 -0300

    Update Part 01.sql

--- Part 01.sql
+++ Part 01.sql
@@ -1,31 +1,31 @@
-# Basic Query  
+-- Basic Query  
 SELECT *
 FROM Customers
 WHERE City = 'São Paulo';
 
-# Sort query 
+-- Sort query 
 SELECT *
 FROM Customers
 WHERE City = 'São Paulo';
 ORDER BY City, CustomerName # First by city then by customer name; 
 
-#Agregate infos by count - How many albuns per style? 
+-- Agregate infos by count - How many albuns per style? 
 SELECT style, COUNT(Name)
 FROM bands
 GROUP BY style
 
-#Agregate infos by avarege - What is the album avarage for each style? 
+-- Agregate infos by avarege - What is the album avarage for each style? 
 SELECT style, AVG(Albuns)
 FROM bands
 GROUP BY style 
 
-#Agregate by avarage and condition - Which estilos have albuns avarage above 10?
+-- Agregate by avarage and condition - Which estilos have albuns avarage above 10?
 SELECT style, AVG(Albuns)
 FROM bands
 GROUP BY style
 HAVING AVG(Albuns) > 10
 
-#- What styles have more than one bands formed before 1970?
+-- What styles have more than one bands formed before 1970?
 SELECT style, COUNT(Name)
 FROM bands
 WHERE Year_Formation < 1970
